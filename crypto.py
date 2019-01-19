b9b89458251f3747fe9345375f7f020ee187820f
Alden Peeters <starbounder1024@gmail.com>
2019-01-19 11:03:20 -0500

    import copy

--- crypto.py
+++ crypto.py
@@ -13,7 +13,7 @@ def sign_row(row):
 
 # Returns true or false
 def verify_row(row, public, signature):
-	obj = oobj.copy()
+	obj = row.copy()
 	signature = base64.b64decode(signature.encode('utf-8'))
 	jstr = json.dumps(obj)
 	verify_key = nacl.signing.VerifyKey(public, encoder=nacl.encoding.URLSafeBase64Encoder)
